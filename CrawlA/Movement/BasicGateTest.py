7cda631ab3395602d78cef3b4d8e6cfb0859fff3
Vertechs <tybi51398@gmail.com>
2016-10-31 23:17:29 -0700

    Update BasicGateTest.py

--- CrawlA/Movement/BasicGateTest.py
+++ CrawlA/Movement/BasicGateTest.py
@@ -1,4 +1,15 @@
 #include os
 #include math
 
-def file_writeout 
+output_file = 'output/mvnt'
+
+def file_writeout(srvN, pos);
+    with open(output_file, 'a') as f:
+        f.write(srvN, ' to ', pos)
+    return 0
+        
+class leg(legN):
+    def __init__(legN):
+        srvHY = 'srv' + legN + 'HY'
+        srvHX = 'srv' + legN + 'HX'
+        srvEY = 'srv' + legN + 'EY'
